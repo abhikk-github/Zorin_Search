0458a3bbe7c97f16f33c275ca8e499b4b4b37575
Abhishek <akk73177@gamil.com>
2021-05-23 18:51:45 +0530

    added

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html itemscope itemtype="http://schema.org/WebPage" lang="en" dir="ltr">
+<head>
+<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+	<title id="title">Start</title>
+
+	<link rel="stylesheet" href="https://assets.zorincdn.com/start/style.css" type="text/css">
+	<link rel="icon" href="https://assets.zorincdn.com/images/favicon.ico">
+	<link href="https://assets.zorincdn.com/vendor/inter/inter.css" rel="stylesheet" type="text/css">
+
+	<script src="https://assets.zorincdn.com/start/scripts/jquery-1.4.2.min.js"></script>
+	<script src="https://assets.zorincdn.com/start/scripts/tools.js"></script>
+	<script src="https://assets.zorincdn.com/start/scripts/scripts.js"></script>
+	<script src="https://assets.zorincdn.com/start/engines/base.js"></script>
+
+	<script async defer data-domain="start.zorin.com" src="https://plausible.zorin.com/js/index.js"></script>
+
+</head>
+<body>
+<div id="engines"></div>
+
+	<div id="container">
+		<form id="form" onsubmit="return doSearch()">
+			<div id="input"><input id="i" type="text" autocomplete="off" spellcheck="false" placeholder="Search" /></div>
+			<a onclick="nextLanguage()" id="lang"></a>
+			<ul id="sugs"></ul>
+		</form>
+
+		<p id="method"></p>
+	</div>
+
+</body>
+</html>
